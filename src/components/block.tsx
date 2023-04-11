42310308f13b1a15c90182b6791dc468aad72c61
Luiz Fernando <ssluizf@gmail.com>
2023-04-10 22:23:37 -0300

    feat(card): increment card's while typing

--- src/components/block.tsx
+++ src/components/block.tsx
@@ -1,12 +1,48 @@
-import Card from "./card";
+import { useState } from "react";
+import Card, { CardObject } from "./card";
 
 export default function Block() {
+  const EMPTY_CARD = { symbol: "", meaning: "" };
+  const [cards, setCards] = useState<CardObject[]>([]);
+
+  function updateCard(card: CardObject, index: number) {
+    setCards((prevState) => {
+      const copyPrevState = [...prevState];
+      const cardIsEmpty = !(card.symbol || card.meaning);
+
+      const currentIsNull = !copyPrevState[index];
+      const cardIsLast = copyPrevState.length - 1 === index;
+
+      const deleteCard = () => {
+        copyPrevState.splice(index, 1);
+
+        return copyPrevState;
+      };
+
+      const editCard = () => {
+        copyPrevState.splice(index, 1, card);
+
+        return copyPrevState;
+      };
+
+      if (cardIsEmpty && currentIsNull) return copyPrevState;
+
+      if (cardIsEmpty && cardIsLast) return deleteCard();
+
+      return editCard();
+    });
+  }
+
   return (
     <div className="">
       <header></header>
       <div className="w-full space-y-4">
-        {Array.from(Array(2).keys()).map((value, index) => (
-          <Card key={`card-${index}`} />
+        {[...cards, EMPTY_CARD].map((card, index) => (
+          <Card
+            key={`card-${index}`}
+            defaultCard={card}
+            updateCard={(cardObj) => updateCard(cardObj, index)}
+          />
         ))}
       </div>
     </div>
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,20 +1,53 @@
-import { useState } from "react";
-import Textarea from "./textarea";
+import { useState, useMemo } from "react";
 
-export default function Card() {
-  const [symbol, setSymbol] = useState("");
-  const [meaning, setMeaning] = useState("");
+import Textarea from "./textarea";
+export interface CardObject {
+  symbol: string;
+  meaning: string;
+}
+
+interface Card {
+  defaultCard: CardObject;
+  updateCard(card: CardObject): void;
+}
+
+export default function Card({ defaultCard, updateCard }: Card) {
+  const [card, setCard] = useState<CardObject>(defaultCard);
+
+  function handleChange(e: any) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    const newCard = { ...card, [name]: value };
+
+    setCard(newCard);
+    updateCard(newCard);
+  }
+
+  function debounce(func: (...args: any) => void, timeout: number) {
+    let timer: any;
+
+    return (...args: any) => {
+      clearTimeout(timer);
+
+      timer = setTimeout(function (this: any) {
+        func.apply(this, args);
+      }, timeout);
+    };
+  }
+
+  const debouncedOnChange = debounce(handleChange, 200);
 
   return (
     <div className="group flex flex-row-reverse w-full space-x-8 space-x-reverse">
-      {(symbol || meaning) && (
+      {(card.symbol || card.meaning) && (
         <>
           <Textarea
             name="meaning"
             placeholder="Add the meaning or translation"
-            className="peer group-last:placeholder:text-transparent"
-            value={meaning}
-            onChange={(e) => setMeaning((e.target as HTMLInputElement).value)}
+            className="peer placeholder:text-transparent group-first:placeholder:text-current"
+            onChange={debouncedOnChange}
+            defaultValue={defaultCard.meaning}
           />
           <div className="flex flex-col h-6">
             <hr className="w-full m-auto px-1 border border-black" />
@@ -24,11 +57,11 @@ export default function Card() {
       <Textarea
         name="symbol"
         placeholder="Add a word, phrase or symbol"
-        className="peer group-last:placeholder:text-transparent"
-        value={symbol}
-        onChange={(e) => setSymbol((e.target as HTMLInputElement).value)}
+        className="peer placeholder:text-transparent group-first:placeholder:text-current"
+        onChange={debouncedOnChange}
+        defaultValue={defaultCard.symbol}
       />
-      <div className="flex flex-col h-6 group-last:invisible group-hover:visible peer-focus:visible">
+      <div className="flex flex-col h-6 group-last:invisible group-only:visible group-hover:visible peer-focus:visible">
         <hr className="w-full m-auto px-1 border border-black" />
       </div>
     </div>
